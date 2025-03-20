6bf54217990d40d9c527730b84509f877152ac5a
fabgal-p1 <>
2025-03-20 19:14:54 +0100

    - add main and repository - get_data and get_fx functions

---
+++ main.py
@@ -0,0 +1,9 @@
+from repository import get_data
+
+
+def main():
+    data = get_data()
+
+
+if __name__ == '__main__':
+    main()
---
+++ repository.py
@@ -0,0 +1,11 @@
+
+def get_data():
+    return [1, 2, 3, 4, 5]
+
+
+def get_fx():
+    return 1
+
+
+def get_accounting():
+    return 1
